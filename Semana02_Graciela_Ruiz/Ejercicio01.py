2a3dea020f8d3c12e4dac0dd1a37fa171adbba6a
GracielaLR <N00560399@upn.pe>
2026-08-31 04:36:23 -0500

    Resolucion del Ejercicio01.py

---
+++ Semana02_Graciela_Ruiz/Ejercicio01.py
@@ -0,0 +1,21 @@
+"""
+============= EJERCICIO 01 =================
+Escribe un programa que solicite la base y 
+altura de un rectángulo y calcule su área 
+(A = base × altura) y su 
+perímetro (P = 2 x (base+altura)).
+Muestra los resultados formateados
+"""
+#Resolucion del ejercicio
+
+base = float(input("Ingresa la base del rectangulo: "))
+altura = float(input("Ingresa la altura del rectangulo: "))
+
+area = base * altura
+perimetro = 2 * ( base + altura )
+
+print("===== RESULTADOS =====\n")
+print(f"Base:       {base:>8.2f}")
+print(f"Altura:     {altura:>8.2f}")
+print(f"Área:       {area:>8.2f}")
+print(f"Perímetro:  {perimetro:>8.2f}")
