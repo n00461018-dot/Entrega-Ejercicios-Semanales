720f4a782baf6c1d77be3c0999b92c5fd91e885e
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-03 09:05:32 -0500

    feat: calculo de precio final luego de un descuento

--- Semana03_Aaron_Gonzales/ejercicio01.py
+++ Semana03_Aaron_Gonzales/ejercicio01.py
@@ -3,3 +3,14 @@ Escribe una función llamada calcular_descuento(precio, porcentaje) que reciba e
 precio original de un producto y el porcentaje de descuento, y retorne el precio
 final después del descuento. Luego muestra el ahorro obtenido
 """
+
+def calcular_descuento(precio, porcentaje):
+    descuento = porcentaje * 0.01
+    ahorro = precio * descuento
+    precioFinal = precio - (ahorro)
+    return precioFinal
+
+precioUser = float(input("Ingrese el precio del producto: "))
+porcDesc = int(input("Ingrese el porcentaje de descuento: " "%"))
+
+print(f"El precio final después del descuento es {calcular_descuento(precioUser, porcDesc)}.")
