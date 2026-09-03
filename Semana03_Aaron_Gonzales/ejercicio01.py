a37a526a1748b5d72ca18c10808fb91f604f3c96
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-03 08:40:45 -0500

    feat: enunciado del ejercicio 1 añadido

---
+++ Semana03_Aaron_Gonzales/ejercicio01.py
@@ -0,0 +1,5 @@
+"""
+Escribe una función llamada calcular_descuento(precio, porcentaje) que reciba el 
+precio original de un producto y el porcentaje de descuento, y retorne el precio
+final después del descuento. Luego muestra el ahorro obtenido
+"""
