b3650d1f45b446e9f643b87945a23d625eda1bc0
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-03 10:09:38 -0500

    feat: agregar validación de número par

--- Semana03_Aaron_Gonzales/ejercicio02.py
+++ Semana03_Aaron_Gonzales/ejercicio02.py
@@ -3,3 +3,19 @@ Crea una función es_par(numero) que retorne True si el número es par o False s
 Luego crea otra función mostrar_paridad(numero) (sin return) que use la primera función e 
 imprima el resultado en pantalla con un mensaje.   
 """
+
+def es_par(numero):
+
+    if numero % 2 == 0:
+        compBool = True
+    else:
+        compBool = False
+        
+    return compBool
+
+def mostrar_paridad(numero):
+    print(f"El número es {es_par(numero)}")
+    
+num = int(input("Ingrese un número: "))
+es_par(num)
+mostrar_paridad(num)
