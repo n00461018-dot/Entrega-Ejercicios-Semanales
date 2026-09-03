031e41980c97a7c1e48b50d53d230e3352a3222c
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-03 13:12:04 -0500

    feat: agregar enunciado del ejercicio 3

---
+++ Semana03_Aaron_Gonzales/ejercicio03.py
@@ -0,0 +1,5 @@
+"""
+Escribe una función calcular_promedio(notas) que reciba una lista de notas y retorne el promedio,
+la nota mínima y la nota máxima. Además crea una función mostrar_resultado(nombre, notas) sin retorno 
+que muestre un reporte formateado
+"""
