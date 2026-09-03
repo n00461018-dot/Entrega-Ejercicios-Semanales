9b00cc2bdddeb9234cf0b63fb01f113c00cbba06
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-03 09:48:44 -0500

    feat: agregar enunciado del ejercicio 2

---
+++ Semana03_Aaron_Gonzales/ejercicio02.py
@@ -0,0 +1,5 @@
+"""
+Crea una función es_par(numero) que retorne True si el número es par o False si es impar.
+Luego crea otra función mostrar_paridad(numero) (sin return) que use la primera función e 
+imprima el resultado en pantalla con un mensaje.   
+"""
