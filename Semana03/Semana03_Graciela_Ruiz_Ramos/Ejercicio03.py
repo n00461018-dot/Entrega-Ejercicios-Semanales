379203af8faee45a816382a9f89240dd070e4579
GracielaLR <N00560399@upn.pe>
2026-09-08 21:13:28 -0500

    Agrego la resolucion del ejercicio 03 de la funcion calculadora de promedio con lista.

---
+++ Semana03/Semana03_Graciela_Ruiz_Ramos/Ejercicio03.py
@@ -0,0 +1,54 @@
+"""
+=========================== EJERCICIO 03 ===========================
+Escribe una función calcular_promedio(notas) que reciba una lista de 
+notas y retorne el promedio, la nota mínima y la nota máxima.
+Además crea una función mostrar_resultado(nombre, notas) sin retorno
+que muestre un reporte formateado.
+"""
+
+#Resolución
+
+def calcular_promedio(notas):
+    promedio = sum(notas) / len(notas)
+    minima = min(notas)
+    maxima = max(notas)
+    return promedio, minima, maxima
+
+
+nombre_usuario = input("Ingresa el nombre del estudiante: ")
+
+while True:
+    try:
+        cantidad_notas = int(input("¿Cuántas notas vas a ingresar?: "))
+        if cantidad_notas >= 0:
+            break
+        print("Error: no se aceptan letras")
+    except ValueError:
+        print("Error: no se aceptan letras")
+
+lista_notas = []
+for i in range(cantidad_notas):
+    while True:
+        try:
+            nota = float(input(f"Ingresa la nota {i + 1} (0 a 20): "))
+        except ValueError:
+            print("Error: no se aceptan letras")
+            continue
+
+        if 0 <= nota <= 20:
+            lista_notas.append(nota)
+            break  # Sale del bucle de validación y pasa a la siguiente nota
+        else:
+            print("Error:Nota fuera de rango. Debe estar entre 0 y 20.")
+
+def mostrar_resultado(nombre, notas):
+    prom, mn, mx = calcular_promedio(notas)
+
+    print(f"\n=== Reporte de Notas ===")
+    print(f"Estudiante       : {nombre}")
+    print(f"Notas ingresadas : {notas}")
+    print(f"Promedio         : {prom:.2f}")
+    print(f"Nota mínima      : {mn}")
+    print(f"Nota máxima      : {mx}")
+
+mostrar_resultado(nombre_usuario, lista_notas)
