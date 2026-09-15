75fcbb98bdbe3c1dfa8da9807533fe51bda7237e
alejandrohdominguez <N00572159@upn.pe>
2026-09-14 23:28:03 -0500

    feat: Mostrar la primera vocal encontrada en contar_vocales

--- Semana04/Semana04_Alejandro_Huilcaya/variables_locales_ej1.py
+++ Semana04/Semana04_Alejandro_Huilcaya/variables_locales_ej1.py
@@ -4,5 +4,7 @@ def contar_vocales(texto):
     for letra in texto:
         if letra in vocales:
             conteo += 1
+            if conteo == 1:
+                print(f"Primera vocal encontrada: {letra}")
     return conteo
 print(contar_vocales("Hola Mundo")) # 4
