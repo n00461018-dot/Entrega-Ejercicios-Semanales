565d4fd860b911d8dd914952dc077e5eedf7443d
alejandrohdominguez <N00572159@upn.pe>
2026-09-15 00:14:17 -0500

    feat: Agregar el total de inventario en funcion mostrar inventario

--- Semana04/Semana04_Alejandro_Huilcaya/variables_globales_ej3.py
+++ Semana04/Semana04_Alejandro_Huilcaya/variables_globales_ej3.py
@@ -5,6 +5,7 @@ def agregar(producto):
 def mostrar():
     for p in inventario:
         print(f" - {p}")
+    print("Total de inventario: ",len(inventario))
 agregar("Laptop")
 agregar("Mouse")
 mostrar()
