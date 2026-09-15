96d9ff055d440309d19ade4fe83b543f8cbc889e
alejandrohdominguez <N00572159@upn.pe>
2026-09-14 23:16:41 -0500

    feat: Agregar funcion para contar las vocales de un texto

---
+++ Semana04/Semana04_Alejandro_Huilcaya/variables_locales_ej1.py
@@ -0,0 +1,8 @@
+def contar_vocales(texto):
+    vocales = "aeiouAEIOU"
+    conteo = 0 # local
+    for letra in texto:
+        if letra in vocales:
+            conteo += 1
+    return conteo
+print(contar_vocales("Hola Mundo")) # 4
