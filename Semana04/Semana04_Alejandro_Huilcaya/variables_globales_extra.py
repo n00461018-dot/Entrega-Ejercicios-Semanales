cfec99e737cbc5ffec40925c47fdf0b0864e6c52
alejandrohdominguez <N00572159@upn.pe>
2026-09-15 00:20:20 -0500

    feat: Agregar funcion agregar_nota usando una lista global

---
+++ Semana04/Semana04_Alejandro_Huilcaya/variables_globales_extra.py
@@ -0,0 +1,8 @@
+# Las listas/dicts se MUTAN sin 'global'
+# (porque no se reasigna la referencia)
+notas = []
+def agregar_nota(n):
+    notas.append(n) # ← sin 'global'
+agregar_nota(95)
+print(notas) # [95]
+
