1c625c19a3cd5796ccc2f287541c879b9dd71d97
Aaron Gonzales <aarongonzalescortez06@gmail.com>
2026-09-15 08:40:39 -0500

    feat: agregar MODO_DEBUG global

---
+++ Semana04/Semana04_Aaron_Gonzales/Ejercicio06.py
@@ -0,0 +1,10 @@
+MODO_DEBUG = True # global (constante)
+
+def procesar(dato):
+    if MODO_DEBUG: # lectura sin 'global'
+        print(f"[DEBUG] Procesando: {dato}")
+    return dato.upper()
+
+procesar("hola") # [DEBUG] Procesando: hola
+
+print(procesar("hola")) #devuelve HOLA
