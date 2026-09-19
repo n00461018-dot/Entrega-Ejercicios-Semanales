38b4c9195264189afa4e593823b56fe0ef2e3a34
alejandrohdominguez <N00572159@upn.pe>
2026-09-18 23:16:42 -0500

    feat: Agregar variables de nota baja, aprobados y la impresion de resultados de funcion estadistica

--- Semana05/semana05_Alejandro_Huilcaya/ej01_estadistica_lista_notas.py
+++ Semana05/semana05_Alejandro_Huilcaya/ej01_estadistica_lista_notas.py
@@ -10,12 +10,16 @@ def estadistica(notas):
     prom=sum(notas)/len(notas)
     n_alta=notas[0]
     n_baja=notas[0]
-
+    aprobados=0
     for nota in notas:
         if nota>n_alta:
             n_alta=nota
-    print(n_alta)
+        if nota<n_baja:
+            n_baja=nota
+        if nota>=11:
+            aprobados+=1
 
+    print(f"Promedio: {prom} | Max: {n_alta} | Min: {n_baja} | Aprobados: {aprobados}")
 
-
+    
 estadistica(notas)
